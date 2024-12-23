360a96ecf61525d256f8880fd8ab249c1d748838
rococorbalan <123517270+rococorbalan@users.noreply.github.com>
2024-12-23 11:56:28 -0300

    Add menu body and elements
    Add function to create a menu element with image
    title and text, append the return element of the
    function to the menu body.

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,10 +1,49 @@
 // menu.js
-function changeToMenu(element, button){
-    const body = document.createElement("div")
-    body.textContent = "menu"
-    element.appendChild(body);
+import grizzlyDrink from "../images/grizzly.png";
+import blackDrink from "../images/black.jpg";
+import koalaDrink from "../images/koala.png";
+import pandaDrink from "../images/panda.png";
+
+function createMenuElement (image, title, text) {
+    const menuElement = document.createElement("div");
+    menuElement.classList.add("menu-element");
+
+    const elementTitle = document.createElement("div");
+    elementTitle.classList.add("element-title");
+    elementTitle.textContent = title;
     
-    button.classList.add("menu")
+    const elementText = document.createElement("div");
+    elementText.classList.add("element-text");
+    elementText.textContent = text;
+
+    const elementImage = document.createElement("img");
+    elementImage.src = image;
+
+    const elementDescription = document.createElement("div");
+    elementDescription.classList.add("element-description");
+
+    elementDescription.appendChild(elementTitle);
+    elementDescription.appendChild(elementText);
+    menuElement.appendChild(elementImage);
+    menuElement.appendChild(elementDescription);
+
+    return menuElement;
+}
+
+function changeToMenu(element, button){
+    button.classList.add("menu");
+
+    const menuBody = document.createElement("div");
+    menuBody.classList.add("menu-body");
+
+    menuBody.appendChild(createMenuElement(grizzlyDrink, "Grizzly Brew", "A bold and earthy espresso-based drink with hints of caramel and cinnamon."));
+    menuBody.appendChild(createMenuElement(blackDrink, "Black Mocha", " A rich dark chocolate mocha infused with a hint of blackberry syrup."));
+    menuBody.appendChild(createMenuElement(pandaDrink, "Panda Matcha", "A creamy matcha latte with a swirl of black sesame for a striking visual."));
+    menuBody.appendChild(createMenuElement(koalaDrink, "Koala Hug", "A soothing eucalyptus and honey herbal tea, perfect for cozy vibes."));
+
+
+    element.appendChild(menuBody)
+
 }
 
 export { changeToMenu };
