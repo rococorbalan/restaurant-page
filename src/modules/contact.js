c0287badefe1e21a15142d882a99a88c4f9be40c
rococorbalan <123517270+rococorbalan@users.noreply.github.com>
2024-12-23 12:31:49 -0300

    Add contact body and images.

--- src/modules/contact.js
+++ src/modules/contact.js
@@ -1,8 +1,51 @@
 // contact.js
+import instagram from "../images/instagram.svg"
+import twitter from "../images/twitter-x.svg"
+
 function changeToContact(element, button){
-    const body = document.createElement("div")
-    body.textContent = "contact"
-    element.appendChild(body);
+    const contactBody = document.createElement("div")
+    contactBody.classList.add("contact-body")
+
+    const addressTitle = document.createElement("div")
+    addressTitle.textContent = "Address"
+    addressTitle.classList.add("content-title");
+
+    const address = document.createElement("div")
+    address.textContent = "123 Woodland Lane. Forest Grove, CA 90210"
+    address.classList.add("content-body");
+
+    const addressDiv = document.createElement("div");
+    addressDiv.appendChild(addressTitle);
+    addressDiv.appendChild(address);
+
+    const phoneTitle = document.createElement("div")
+    phoneTitle.classList.add("content-title");
+    phoneTitle.textContent = "Phone Number"
+
+    const phoneNumber = document.createElement("div")
+    phoneNumber.textContent = "(555) 987-6543"
+    phoneNumber.classList.add("content-body");
+
+    const phoneDiv = document.createElement("div");
+    phoneDiv.appendChild(phoneTitle);
+    phoneDiv.appendChild(phoneNumber);
+
+    const socials = document.createElement("div");
+    socials.classList.add("socials")
+
+    const instagramLogo = document.createElement("img")
+    instagramLogo.src = instagram;
+    const twitterLogo = document.createElement("img")
+    twitterLogo.src = twitter;
+
+    socials.appendChild(instagramLogo);
+    socials.appendChild(twitterLogo);
+
+    contactBody.appendChild(addressDiv);
+    contactBody.appendChild(phoneDiv);
+    contactBody.appendChild(socials);
+
+    element.appendChild(contactBody);
 
     button.classList.add("contact");
 }
